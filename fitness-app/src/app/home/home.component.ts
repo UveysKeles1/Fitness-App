701fb1fb836c0e9c1a85c634187ef77f936c0fd9
UveysKeles <s1081811@stud.sbg.ac.at>
2024-03-07 13:40:26 +0100

    add Home-toolbar

--- fitness-app/src/app/home/home.component.ts
+++ fitness-app/src/app/home/home.component.ts
@@ -1,10 +1,14 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { ButtonModule } from 'primeng/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { MatToolbarModule } from '@angular/material/toolbar';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, MatToolbarModule, MatButtonModule, MatIconModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css',
 })
