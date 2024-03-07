31e3c973d33a08c9f67660065f403cd46b6aa6f7
UveysKeles <s1081811@stud.sbg.ac.at>
2024-03-07 16:55:16 +0100

    resolved comments

--- fitness-app/src/app/app.component.ts
+++ fitness-app/src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { HomeComponent } from './home/home.component';
+import { HeaderComponent } from './navigation/header/header.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, HomeComponent],
+  imports: [CommonModule, HomeComponent, HeaderComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
